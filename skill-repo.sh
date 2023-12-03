d80fba904b51a3b429287f502d62e6478735cf9d
Nikolay Smirnov <justnikobird@yandex.ru>
2023-12-03 17:51:34 +0300

    fix

--- skill-repo.sh
+++ skill-repo.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 if [[ "${UID}" -ne 0 ]]; then
   echo "You need to run this script as root!"
   exit 1
@@ -40,17 +38,28 @@ command_check rngd "Rng-tools" rng-tools
 command_check nginx "Nginx" nginx
 command_check htpasswd "Htpasswd" apache2-utils
 command_check basename "Basename" coreutils
-
+command_check bzip2 "Bzip2" bzip2
 
 echo -e "\n====================\nAptly Configuration...\n====================\n"
-wget -P /tmp/ https://github.com/aptly-dev/aptly/releases/download/v1.5.0/aptly_1.5.0_linux_amd64.tar.gz
-tar xvf /tmp/aptly_1.5.0_linux_amd64.tar.gz
-mv /tmp/aptly_1.5.0_linux_amd64/aptly /usr/local/bin/
+
+if [ -f /tmp/aptly_1.5.0_linux_amd64.tar.gz ] || [ -d /tmp/aptly_1.5.0_linux_amd64 ]; then
+  rm -rf /tmp/aptly_1.5.0_linux_amd64*
+fi
+
+if wget -P /tmp/ https://github.com/aptly-dev/aptly/releases/download/v1.5.0/aptly_1.5.0_linux_amd64.tar.gz; then
+  tar -xvf /tmp/aptly_1.5.0_linux_amd64.tar.gz -C /tmp/
+  mv -f /tmp/aptly_1.5.0_linux_amd64/aptly /usr/local/bin/
+else
+  exit 1
+fi
+rm -rf /opt/aptly
+rm -rf /var/www/aptly
+
 echo '{
   "rootDir": "/opt/aptly",
   "downloadConcurrency": 4,
   "downloadSpeedLimit": 0,
-  "architectures": [],
+  "architectures": ["all","amd64"],
   "dependencyFollowSuggests": false,
   "dependencyFollowRecommends": false,
   "dependencyFollowAllVariants": false,
@@ -71,20 +80,32 @@ echo '{
     }
   },
   "enableMetricsEndpoint": false
-}' > /etc/aptly.conf
+}' >/etc/aptly.conf
 
-aptly repo create -comment="lab repo" -component="main" -distribution="focal" lab
-
-package_path=$(path_request "first package")
-aptly repo add lab "$package_path"
+if aptly repo create -comment="lab repo" -component="main" -distribution="focal" lab; then
+  package_path=$(path_request "first package")
+  if ! aptly repo add lab "$package_path"; then
+    exit 1
+  fi
+else
+  exit 1
+fi
 
 rngd -r /dev/urandom
-gpg --default-new-key-algo rsa4096 --gen-key --keyring pubring
-gpg --list-keys
+rngd_check=$?
+if [ $rngd_check -eq 0 ] || [ $rngd_check -eq 10 ]; then
+  gpg --default-new-key-algo rsa4096 --gen-key --keyring pubring
+  gen_key_check=$?
+  if [ $gen_key_check -eq 0 ] || [ $gen_key_check -eq 2 ]; then
+    gpg --list-keys
+  fi
+else
+  exit 1
+fi
 
 aptly publish repo lab filesystem:lab:lab
 
-gpg --export --armor > /var/www/aptly/lab/pubtest.asc
+gpg --export --armor >/var/www/aptly/lab/pubtest.asc
 
 ca_crt=$(path_request "ca certificate")
 cp "$ca_crt" /var/www/aptly/lab/
@@ -104,7 +125,7 @@ key_file=$(basename "$server_key")
 read -r -p $'\n\n'"login for ${repo_name} (default nikolay): " repo_login
 read -r -p "password for ${repo_name} (default Sm1rn0V187187): " -s repo_pass
 
-htpasswd -nbB -C 10 "$repo_login" "$repo_pass" >> /etc/nginx/conf.d/.htpasswd
+htpasswd -nbB -C 10 "$repo_login" "$repo_pass" >>/etc/nginx/conf.d/.htpasswd
 
 echo '
 server {
@@ -133,7 +154,7 @@ server {
                 allow 127.0.0.1;
                 deny all;
         }
-}'
+}' >/etc/nginx/sites-available/default
 
 echo -e "\n====================\nIptables configuration\n====================\n"
 iptables_add INPUT -p tcp --dport 1111 -j ACCEPT -m comment --comment repo_nginx
@@ -146,5 +167,3 @@ systemctl enable nginx.service
 
 echo -e "\n====================\nRepo listening on port 1111\n====================\n"
 echo -e "\nOK\n"
-
-
