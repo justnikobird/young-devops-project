27bc3a7e2c5b83242ef7b6e8b7ccad12db574562
Nikolay Smirnov <justnikobird@yandex.ru>
2024-01-28 11:05:38 +0300

    fix

--- vm-start.sh
+++ vm-start.sh
@@ -122,8 +122,10 @@ while true; do
       [Cc]*)
         # запросим имя vm
         read -r -p $'\n'"vm name: " vm_name
+
         # выполним корректировку prompt statement
         echo "PS1='\${debian_chroot:+(\$debian_chroot)}\\u@$vm_name:\\w\\\$ '" >>/home/"$username"/.bashrc
+
         echo -e "\n\nDONE\n"
         break
         ;;
